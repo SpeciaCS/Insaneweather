4a245bfd49d642ecf35c6be8036cf5fdafdaa1d6
15188355273 <liu15188355273@163.com>
2019-03-30 11:59:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/insaneweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.example.insaneweather.db;
+
+import org.litepal.exceptions.DataSupportException;
+
+public class Province extends DataSupportException {
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public Province(String errorMessage, Throwable throwable) {
+        super(errorMessage, throwable);
+    }
+}
